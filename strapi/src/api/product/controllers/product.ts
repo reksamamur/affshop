3b45f8372c56254e73e601d7cd9a8b6576ac7e4b
reksamamur <reksa.kuana9@gmail.com>
2025-07-17 23:00:18 +0700

    update product controller

--- strapi/src/api/product/controllers/product.ts
+++ strapi/src/api/product/controllers/product.ts
@@ -2,6 +2,27 @@
  * product controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController(
+  'api::product.product',
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+
+      const query = {
+        filters: { slug },
+        ...ctx.query,
+      };
+
+      const product = await strapi.entityService.findMany(
+        'api::product.product',
+        query
+      );
+
+      const sanitizedEntity = await this.sanitizeOutput(product, ctx);
+
+      return this.transformResponse(sanitizedEntity[0]);
+    },
+  })
+);
